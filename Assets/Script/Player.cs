fb5854f4a2478f746bde5bf9fd519082d224374c
GooKu <kuwoods@hotmail.com>
2014-08-02 17:05:40 +0800

    update Player.cs

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,14 +5,19 @@ public class Player : MonoBehaviour {
 
 	int speed=1;
 	int jumpForce = 450;
+	float attack_time_limit = 0.2f;
+	float attack_CDtime_limit = 0.5f;
+	float attacking_time, attack_CDtime;
 	string player_name;
+	Animator anim;
 
-	public enum Player_Status {Moving, Jump, Jump2, Dead, Pass};
+	public enum Player_Status {Moving, Jump, Jump2, Attack, Dead, Pass};
 	Player_Status player_status = Player_Status.Moving;
 
 	void Start () {
 		player_status = Player_Status.Moving;
 		player_name = gameObject.name;
+//		anim = gameObject.GetComponent<Animator>();
 	}
 	
 	void Update () {
@@ -53,6 +58,16 @@ public class Player : MonoBehaviour {
 			Jump();
 			break;
 		case "Hero3":
+			if(player_status != Player_Status.Attack){
+				if(attack_CDtime > 0)
+				{
+					attack_CDtime -= Time.deltaTime;
+				}else{
+					attack_CDtime = attack_CDtime_limit;
+					Attack();
+				}
+			}
+
 			break;
 		case "Hero4":
 			break;
@@ -73,7 +88,13 @@ public class Player : MonoBehaviour {
 			player_status = Player_Status.Jump;
 	}
 
+	void Attack(){
+		if (attacking_time == 0) {
+			player_status = Player_Status.Attack;
+		}
 
+		attacking_time += Time.deltaTime;
+	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
 

fdf714e1739c40cc5d7d9a7a1c1d5e64b18c7558
Cliff Lee <tommyboys0107@gmail.com>
2014-08-02 15:42:51 +0800

    Camera follow

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public float height;
+	public float width;
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+		Follow();
+		CheckBoundary();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Follow();
+		CheckBoundary();
+	}
+
+	void Follow(){
+		transform.position = new Vector3(player.transform.position.x, 
+		                                 player.transform.position.y, 
+		                                 transform.position.z);
+	}
+
+	void CheckBoundary(){
+		if(!IsWithinBounds()){
+			MakeWithinBounds();
+		}
+	}
+
+	bool IsWithinBounds(){
+		
+	}
+
+	void MakeWithinBounds(){
+	
+	}
+}

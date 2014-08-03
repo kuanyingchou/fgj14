bd8ecddb5f559572b70a5dd0cf083fcd8a0c27cf
ken <kuanyingchou@gmail.com>
2014-08-03 15:41:31 +0800

    add monster movement

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -3,15 +3,13 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	/*// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}*/
+    IEnumerator Start () {
+        while(true) 
+        {
+            yield return new WaitForSeconds(Random.Range(.1f, 1f));
+            iTween.RotateBy(gameObject, new Vector3(0, 0, -.1f), 1);
+        }
+    }
 
 	void OnCollisionStay2D(Collision2D col){
 		if(col.gameObject.tag == "Weapon" && Player.player_status == Player.Player_Status.Attack)

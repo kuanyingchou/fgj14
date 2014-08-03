de4bf276cfa3c933c4c91004db147dea56fa6c40
ken <kuanyingchou@gmail.com>
2014-08-03 14:24:59 +0800

    add back to title

---
+++ Assets/Script/BackToTitle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToTitle : MonoBehaviour {
+    public void Update()
+    {
+        if (Input.GetKeyDown ("space"))
+        {
+            Application.LoadLevel("Title");
+        }
+    }
+}
